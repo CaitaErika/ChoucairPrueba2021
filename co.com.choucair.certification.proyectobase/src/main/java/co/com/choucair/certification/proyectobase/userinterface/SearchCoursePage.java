e44754fb67a6a3b0b226d6a2ab79825bfba01238
CaitaErika <saludsoftss@gmail.com>
2021-06-04 13:21:21 -0500

    up
    últimos cambios

---
+++ co.com.choucair.certification.proyectobase/src/main/java/co/com/choucair/certification/proyectobase/userinterface/SearchCoursePage.java
@@ -0,0 +1,13 @@
+package co.com.choucair.certification.proyectobase.userinterface;
+
+import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.screenplay.targets.Target;
+import org.openqa.selenium.By;
+
+public class SearchCoursePage extends PageObject {
+    public static final Target BUTTON_UC = Target.the("Selecciona la certificacion choucair").located(By.xpath("//div[@id='certificaciones']//strong"));
+    public static final Target INPUT_COURSE = Target.the("Busca el curso").located(By.id("coursesearchbox"));
+    public static final Target BUTTON_GO = Target.the("Clic para buscar el curso").located(By.className("btn btn-secondary"));
+    public static final Target SELECT_COURSE = Target.the("Clic para buscar el curso").located(By.xpath("//h4[contains(text()'Foundation Level')]"));
+
+}
---
+++ co.com.choucair.certification.proyectobase/src/test/resources/features/academyChoucair.feature
@@ -0,0 +1,9 @@
+# Author Erika Isabel Caita Avila
+  @stories
+  Feature: Academy Choucair
+    As a User, I want to learn how to automate in screenplay at the choucair Academy with the automation course
+    @scenario1
+    Scenario: Search for automation course
+      Given than santiago wants to learn automation at the academy choucair
+      When he search for the course Foundation Level on the choucair academy platform
+      Then he finds the course called resources Foundation Level
